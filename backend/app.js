927974de2dc30e566837929c55cfc7f7c26bf1b0
Julien2195 <j.lemaitre24@gmail.com>
2022-12-20 16:06:33 +0100

    ajout du model User, et  ajout dans app.js

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,16 @@
 // Importation du framework express
 const express = require('express');
+const morgan = require('morgan')
 const mongoose = require("./database/database");
-
+const User = require("./models/User")
 const bodyParser = require("body-parser");
 
 //Configuration de express
 const app = express();
+
+// Permets de voir les couleurs: vert pour reussite, rouge pour erreur et jaune code erreur client.
+
+app.use(morgan("dev"))
 
 app.use(bodyParser)
 
